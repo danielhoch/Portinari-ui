647a97ade7642eab7effb3ac9d8828ced24f0c21
Daniel Herbert Hoch <danielhoch28@gmail.com>
2020-02-02 21:39:36 -0300

    Adicionando projeto criado com Portinari-ui

---
+++ portinari/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { PoMenuItem } from '@portinari/portinari-ui';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Configurações', action: this.onClick2.bind(this) }
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
+  private onClick2() {
+    alert('Clicked in menu Configurações')
+  }
+
+}
